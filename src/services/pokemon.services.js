7bf261655bccfc7834640cdbd3fa487077af8893
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-07 20:43:39 -0300

    criar service que busca apenas um pokemon

--- src/services/pokemon.services.js
+++ src/services/pokemon.services.js
@@ -1,11 +1,20 @@
-import pokemon from '../db/models/pokemon';
+import pokemonModel from '../db/models/pokemon';
 
-// eslint-disable-next-line
 export const GetPokemons = async (obj = {}) => {
   try {
-    const pokemons = await pokemon.find(obj);
+    const pokemons = await pokemonModel.find(obj);
 
     return pokemons;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const GetOnePokemon = async (obj = {}) => {
+  try {
+    const pokemon = await pokemonModel.findOne(obj);
+
+    return pokemon;
   } catch (err) {
     throw new Error(err);
   }
