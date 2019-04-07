729c0ee2e960a26be1179d1734960dcacf944035
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-07 15:43:52 -0300

    ajustes do eslint

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv'
-
-dotenv.config();
+import dotenv from 'dotenv';
 
 import express from 'express';
 import mongoose from 'mongoose';
 
 import db from './db/db';
 
+dotenv.config();
+
 const app = express();
 const port = process.env.port || '8080';
 
-mongoose.connect(db, { useCreateIndex: true, useNewUrlParser: true})
+mongoose.connect(db, { useCreateIndex: true, useNewUrlParser: true })
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.log(err));
 
